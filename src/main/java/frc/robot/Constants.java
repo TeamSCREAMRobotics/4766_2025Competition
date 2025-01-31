3f06ac56def57ffd754ac491f9ee409308e24531
wanted3306 <100186141+mashed26@users.noreply.github.com>
2025-01-31 17:27:44 -0600

    Added All Files

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+public class Constants {
+
+    public static final class ClimberConstants{
+    
+    }
+
+    public static final class ElevatorConstants{
+    
+    }
+
+    public static final class IntakeConstants{
+    
+    }
+
+    public static final class ManipulatorConstants{
+    
+    }
+}
---
+++ src/main/java/frc/robot/controls/Controls.java
@@ -0,0 +1,11 @@
+package frc.robot.controls;
+
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+public class Controls {
+    public static CommandXboxController driverCon = new CommandXboxController(0);
+    public static CommandXboxController opCon = new CommandXboxController(1);
+    public static Buttonboard buttonboard = new Buttonboard(2);
+
+    
+}
