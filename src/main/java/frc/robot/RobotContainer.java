24c4582e3797bb9970b148e2a1cfa27f636145f4
wanted3306 <100186141+mashed26@users.noreply.github.com>
2025-02-26 01:17:49 -0600

    Added PathPlanner Commands and one auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,12 +7,22 @@ package frc.robot;
 import static edu.wpi.first.units.Units.*;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
 import dev.doglog.DogLog;
 import dev.doglog.DogLogOptions;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.Elevator.runElevator;
+import frc.robot.commands.Intake.intakeSpitOut;
+import frc.robot.commands.Manipulator.manipIntake;
+import frc.robot.commands.Manipulator.manipOuttake;
+import frc.robot.commands.Manipulator.manipPivot;
+import frc.robot.constants.Constants.ElevatorConstants;
+import frc.robot.constants.Constants.ManipulatorConstants;
 import frc.robot.constants.TunerConstants;
 import frc.robot.controls.Controls;
 import frc.robot.subsytems.Climber;
@@ -43,10 +53,27 @@ public class RobotContainer {
   /* Setting up bindings for necessary control of the swerve drive platform */
 
   public RobotContainer() {
-    // Note that X is defined as forward according to WPILib convention,
-    // and Y is defined as to the left according to WPILib convention.
+    // Setting the Trough to work with Autos
+    NamedCommands.registerCommand("Trough", new intakeSpitOut(s_Intake, 0));
+
+    // Setting the Elevator to work with Autos
+    NamedCommands.registerCommand(
+        "L2",
+        new runElevator(s_Elevator, ElevatorConstants.L2Setpoint)
+            .alongWith(new manipPivot(s_Manipulator, ManipulatorConstants.manipSetpoint)));
+    NamedCommands.registerCommand(
+        "L3",
+        new runElevator(s_Elevator, ElevatorConstants.L3Setpoint)
+            .alongWith(new manipPivot(s_Manipulator, ManipulatorConstants.manipSetpoint)));
+    
+    // Setting the Manip Commands to work with Autos
+    NamedCommands.registerCommand("ManipIntake", new manipIntake(s_Manipulator));
+    NamedCommands.registerCommand("ManipOuttake", new manipOuttake(s_Manipulator));
+    NamedCommands.registerCommand("MaipPivot", new manipPivot(s_Manipulator, ManipulatorConstants.manipSetpoint));
 
     auto = AutoBuilder.buildAutoChooser();
+
+    auto.addOption("testAuto", new PathPlannerAuto("Test Auto"));
 
     SmartDashboard.putData(auto);
 
