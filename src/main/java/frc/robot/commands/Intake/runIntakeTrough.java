85992bc2981272d704a08264f516c991949f0ed5
Cash Lewis <lewis.cash@sedalia200.org>
2025-03-05 13:40:42 -0600

    Change timer.start to initialize

--- src/main/java/frc/robot/commands/Intake/runIntakeTrough.java
+++ src/main/java/frc/robot/commands/Intake/runIntakeTrough.java
@@ -24,6 +24,7 @@ public class runIntakeTrough extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    timer.start();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
